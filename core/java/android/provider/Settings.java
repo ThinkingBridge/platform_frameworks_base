ed0d8854b0f7f591baffdc5573f311f9b7825f66
László Dávid <laszlo.david@gmail.com>
2012-12-16 21:57:16 +0100

    Status bar brightness control from CM10 (Frameworks portion)
    Change-Id: Ida526bf19e56d549f7cc75c47466e6953295d888
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2486,6 +2486,13 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
+         /**
+         * Whether to control brightness from status bar
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
