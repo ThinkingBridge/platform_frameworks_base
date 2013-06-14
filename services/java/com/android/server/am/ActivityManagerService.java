e2701d3dd95a5f83e176189a58e4d3e8fbb92859
htchoi1995 <htchoi1995@gmail.com>
2013-06-14 15:32:21 +0900

    Revert "remove some lag launching apps and improve scrolling"
    This reverts commit 85c2a6d4eb27378548b243afdfad108f12a37d77.

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -3184,7 +3184,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
     }
 
-    final void logAppTooSlow(int pid, long startTime, String msg) {
+    final void logAppTooSlow(ProcessRecord app, long startTime, String msg) {
         if (true || IS_USER_BUILD) {
             return;
         }
@@ -3223,7 +3223,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 sb.append(msg);
                 fos = new FileOutputStream(tracesFile);
                 fos.write(sb.toString().getBytes());
-                if (pid <= 0) {
+                if (app == null) {
                     fos.write("\n*** No application process!".getBytes());
                 }
             } catch (IOException e) {
@@ -3240,9 +3240,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                 }
             }
 
-            if (pid > 0) {
+            if (app != null) {
                 ArrayList<Integer> firstPids = new ArrayList<Integer>();
-                firstPids.add(pid);
+                firstPids.add(app.pid);
                 dumpStackTraces(tracesPath, firstPids, null, null, null);
             }
 
