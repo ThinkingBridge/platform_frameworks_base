215ad8d7f8c0c26c20cb7bb8b61bc3d4c65bbc1c
jkl5616 <jkl5616@gmail.com>
2013-06-22 13:31:24 +0900

    Add listview interpolators

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4875,6 +4875,19 @@ public final class Settings {
          * 8 == Translate (Right)
          */
          public static final String LISTVIEW_ANIMATION = "listview_animation";
+
+        /**
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         */
+         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
     }
 
     /**
--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -77,6 +77,7 @@ import android.view.animation.Animation;
 import android.view.animation.AlphaAnimation;  
 import android.view.animation.ScaleAnimation;  
 import android.view.animation.TranslateAnimation;  
+import android.view.animation.AnimationUtils;
 
 /**
  * Base class that can be used to implement virtualized lists of items. A list does
@@ -2266,9 +2267,35 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 break;
         }
         anim.setDuration(500);
+        int mInterpolator = Settings.System.getInt(mContext.getContentResolver(),Settings.System.LISTVIEW_INTERPOLATOR, 0);
+        switch (mInterpolator) {
+            case 1:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.accelerate_interpolator);
+                break;
+            case 2:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.decelerate_interpolator);
+                break;
+            case 3:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.accelerate_decelerate_interpolator);
+                break;
+            case 4:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.anticipate_interpolator);
+                break;
+            case 5:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.overshoot_interpolator);
+                break;
+            case 6:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.anticipate_overshoot_interpolator);
+                break;
+            case 7:
+                anim.setInterpolator(loadInterpolator(this, android.R.anim.bounce_interpolator);
+                break;
+        }      
         view.startAnimation(anim);
         return view;
     }
+ 
+    
 
     class ListItemAccessibilityDelegate extends AccessibilityDelegate {
         @Override
