89fe7aed686a2fbba12d9dd1e3572bc019a01046
htchoi1995 <htchoi1995@gmail.com>
2013-06-30 22:48:50 +0900

    QuickSettings: LongClick Action to Sleep Tile

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/SleepToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/SleepToggle.java
@@ -3,7 +3,9 @@ package com.android.systemui.statusbar.toggles;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.view.View;
+import android.view.KeyEvent;
 import android.os.PowerManager;
 import android.os.SystemClock;
 
@@ -24,4 +26,26 @@ public class SleepToggle extends BaseToggle {
         pm.goToSleep(SystemClock.uptimeMillis());
     }
 
+    @Override
+    public boolean onLongClick(View v) {
+        triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+        return super.onLongClick(v);
+    }
+
+    private void triggerVirtualKeypress(final int keyCode, final boolean longPress) {
+        new Thread(new Runnable() {
+            public void run() {
+                InputManager im = InputManager.getInstance();
+                KeyEvent keyEvent;
+                if (longPress) {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_LONG_PRESS);
+                } else {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_UP, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM);
+                }
+                im.injectInputEvent(keyEvent, InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT);
+            }
+        }).start();
+    }
 }
