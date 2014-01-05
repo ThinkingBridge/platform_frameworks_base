5684170dd1bff86e851a9d10f70e703253544458
Lars Greiss <kufikugel@googlemail.com>
2014-01-05 02:22:53 +0100

    QS: let's welcome LocationTile to 4.4
    we already modified to use the basic 4.4 location mechanism. Still
    it was very basic. Now using the same attempt like in Settings to
    controll location mode (high accuracy, battery saving, device only) and on/off state
    
    longpress switches between the states. Normal click turns it on/off
    Compared to settings turning on will recover last state where it was turned off. As
    well it respects the sharing location warning dialog either when you switch on into
    a mode which use it or you switch into a mode which uses it and it was not confimed.
    
    Slight corrections to original AOSP code to reflect the changes in LocationController
    even though this code is non operational
    
    PatchSet:
    - moved the complete handling into the LocationController. Way better
      to have in centralized. Who knows for what we will use it later.
    PatchSet:
    - cleanup
    
    PatchSet:
    - added drawables from kroz :)
    
    Change-Id: I3c7c51c8d7e3a8c003c37d1b55a6503a9bbeaf64

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -821,7 +821,8 @@ class QuickSettings {
     }
 
     private void applyLocationEnabledStatus() {
-        mModel.onLocationSettingsChanged(mLocationController.isLocationEnabled());
+        mModel.onLocationSettingsChanged(
+                mLocationController.isLocationEnabled(), mLocationController.getLocationMode());
     }
 
     void reloadUserInfo() {
