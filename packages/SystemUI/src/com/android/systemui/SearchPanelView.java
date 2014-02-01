ef9140093111b2ce85e8efffb1fa42085e9a6d67
htchoi1995 <htchoi1995@gmail.com>
2014-02-01 23:48:12 +0900

    fb: allow to recreate statusbar, navbar and notification drawer

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -554,9 +554,9 @@ public class SearchPanelView extends FrameLayout implements
             return null;
         }
         Drawable iconBg = ColorHelper.resize(mContext,
-            mResources.getDrawable(R.drawable.ic_navbar_blank), 50);
+            mResources.getDrawable(R.drawable.ic_navbar_blank), 60);
         Drawable iconBgActivated = ColorHelper.resize(mContext,
-            mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 50);
+            mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 60);
         int margin = (int)(iconBg.getIntrinsicHeight() / 3);
         LayerDrawable icon = new LayerDrawable (new Drawable[] {iconBg, activityIcon});
         icon.setLayerInset(1, margin, margin, margin, margin);
